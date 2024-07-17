8971cd5169d7ad1f8ac394a53dfe91a6b3305c50
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-07-17 08:25:06 +0200

    Working through the basics

--- app.js
+++ app.js
@@ -3,27 +3,68 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-
 var app = express();
+require("dotenv").config();
+
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URL;
+
+// wait for db to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-const mongoDB = process.env.MONGODB_URL;
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// setting up LocalStrategy
+
+// handle route for get index
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// handle route for get signup form
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up-form");
+});
+
+// handle POST signup
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const result = await user.save();
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
